abad0781826c6c360cf8a1c3a402110bee644fe5
Nathan Pitman <nathan@ninefour.co.uk>
2012-07-23 13:11:24 +0100

    updated instructions

--- system/expressionengine/third_party/crib/pi.crib.php
+++ system/expressionengine/third_party/crib/pi.crib.php
@@ -177,31 +177,11 @@ class Crib {
 		?>
 			This plugin cribs content from one or more URLs provided in a plain text file and inserts the collected data as channel entries.
 
-			To use this plugin, wrap anything you want to be processed by it between these tag pairs:
+			To use this plugin, add the following tag to a page template:
 
-			{exp:xml_encode}
-
-			text you want processed
-
-			{/exp:xml_encode}
-
-			Note: Because quotes are converted into &quot; by this plugin, you cannot use
-			ExpressionEngine conditionals inside of this plugin tag.
-
-			If you have existing entities in the text that you do not wish to be converted, you may use
-			the parameter protect_entities="yes", e.g.:
-
-			{exp:xml_encode}Text &amp; Entities{/exp:xml_encode}
-
-			results in: Text &amp;amp; Entities
-
-			{exp:xml_encode protect_entities="yes"}Text &amp; Entities{/exp:xml_encode}
-
-			results in: Text &amp; Entities
-	
-			Version 1.3
-		******************
-		- Updated plugin to be 2.0 compatible
+			{exp:crib url_list_path="http://www.example.com/urllist.txt" channel_id="1" category_ids="1" body_field_id="1" body_field_format="xhtml"}
+			
+			urllist.txt should be a simple text file with fully qualified URLs on separate lines.
 
 		<?php
 		$buffer = ob_get_contents();
